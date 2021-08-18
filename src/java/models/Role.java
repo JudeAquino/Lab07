c49704d67ff00528a5679a0a2f2fab0540b56948
JudeAquino <aquino_judox16@yahoo.com>
2021-08-18 02:36:28 -0600

    complete changes

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,9 +1,16 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package models;
 
 import java.io.Serializable;
 
-
+/**
+ *
+ * @author 468181
+ */
 public class Role implements Serializable{
 
     public static void add(Role role) {
--- web/WEB-INF/users.jsp
+++ web/WEB-INF/users.jsp
@@ -1,3 +1,8 @@
+<%-- 
+    Document   : users
+    Created on : Mar 10, 2021, 9:01:04 AM
+    Author     : 468181
+--%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
